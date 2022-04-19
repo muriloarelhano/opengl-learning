28bab52bb77aa19bfede878b5f0ae2a72ae7d697
Murilo Esteca Arelhano <51941771+muriloarelhano@users.noreply.github.com>
2022-04-18 21:33:31 -0400

    create new window

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,54 @@
-#include <iostream>
+#include <stdio.h>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+
+	if (!glfwInit()) {
+		printf("GLFW initialisation failed!");
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	GLFWwindow* mainWindow = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
+	if (!mainWindow) {
+		printf("GLFW window creation failed!");
+		glfwTerminate();
+		return -1;
+	}
+
+	int bufferWidth, bufferHeight;
+	glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+	glfwMakeContextCurrent(mainWindow);
+
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK) {
+		printf("GLEW initialisation failed!");
+		glfwDestroyWindow(mainWindow);
+		glfwTerminate();
+		return -1;
+	}
+
+	printf("OpenGL version: %s\n", glGetString(GL_VERSION));
+
+	glViewport(0, 0, bufferWidth, bufferHeight);
+
+	while (!glfwWindowShouldClose(mainWindow)) {
+
+		glfwPollEvents();
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(mainWindow);
+	}
+
+	return 0;
 }
